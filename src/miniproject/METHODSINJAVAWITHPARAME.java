4466dc5f97d836adedc499303e4c58d2c1d792de
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-23 19:50:52 +0400

    Update METHODSINJAVAWITHPARAME.java

--- src/miniproject/METHODSINJAVAWITHPARAME.java
+++ src/miniproject/METHODSINJAVAWITHPARAME.java
@@ -1,17 +1,1 @@
-package miniproject;
 
-import java.util.Scanner;
-
-public class METHODSINJAVAWITHPARAME {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        display("musaib",6 );
-    }
-    public static void display(String name,int number){
-        int i=1;
-        while(i<number){
-            System.out.println(i + " " + name);
-            i++;
-        }
-    }
-}
